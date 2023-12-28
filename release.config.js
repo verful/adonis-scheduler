e94124fb957c8c9a0cef7a2dde7ffb53c34baf3a
Arthur Rezende <arthur-er@protonmail.com>
2023-12-28 02:50:08 -0300

    feat: setup semantic-release git plugin

--- release.config.js
+++ release.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   branches: ['main'],
+  plugins: [
+    '@semantic-release/git',
+    {
+      assets: ['package.json'],
+      message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+    },
+  ],
 }
